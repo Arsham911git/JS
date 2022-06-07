b364043467ea8fa27ac9483657d958a1d3999f1b
Arsham911git <99877942+Arsham911git@users.noreply.github.com>
2022-06-07 20:11:52 +1000

    сдвиг влево

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $.getJSON("/JSON/motorists.json", json => {
-        let motorist = '';
-        $.each(json, function (_key, value) {
-            motorist +=
-                `<tr>
-                    <td>${value.person.firstname} ${value.person.lastname}</td>
-                    <td>${value.car.manufacturer}</td>
-                    <td>${value.car.model}</td>
-                    <td>${value.car.year}</td>
-                </tr>`;
-        });
-        $('.table').append(motorist);
+    let motorist = '';
+    $.each(json, function (_key, value) {
+        motorist +=
+        `<tr>
+            <td>${value.person.firstname} ${value.person.lastname}</td>
+            <td>${value.car.manufacturer}</td>
+            <td>${value.car.model}</td>
+            <td>${value.car.year}</td>
+        </tr>`;
     });
+    $('.table').append(motorist);
+});
