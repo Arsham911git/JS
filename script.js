d247956236b28dbbe5f1c8010d4a2ab8bb7c0fcf
Arsham911git <99877942+Arsham911git@users.noreply.github.com>
2022-06-07 10:06:22 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$.getJSON("/JSON/motorists.json",
+    function (json) {
+        let motorist = '';
+        $.each(json, function (_key, value) {
+            motorist +=
+                `<tr>
+                    <td>${value.person.firstname} ${value.person.lastname}</td>
+                    <td>${value.car.manufacturer}</td>
+                    <td>${value.car.model}</td>
+                    <td>${value.car.year}</td>
+                </tr>`;
+        });
+        $('.table').append(motorist);
+    });
